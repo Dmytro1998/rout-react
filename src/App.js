ecb17b00a8a09833e471dc518bc195e5fda75f59
Dmytro <evdokimov041998@gmail.com>
2021-10-28 18:16:42 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React from "react";
-import Manatee from "./components/Manatee/Manatee";
-import Narwhal from "./components/Narwhal/Narwahl"
-import Whale from "./components/Whale/Whale";
+import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './index.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import Manatee from '../src/components/Manatee/Manatee';
+import Narwhal from '../src/components/Narwhal/Narwahl';
+import Whale from '../src/components/Whale/Whale';
 
 function App() {
   return (
     <div className="wrapper">
       <h1>Marine Mammals</h1>
-      <nav>
-        <ul>
-          <li><a href="/manatee">Manatee</a></li>
-          <li><a href="/narwhal">Narwhal</a></li>
-          <li><a href="/whale">Whale</a></li>
-        </ul>
-      </nav>
       <BrowserRouter>
-      <Route path="/manatee">
+        <nav>
+          <ul>
+            <li><Link to="/manatee">Manatee</Link></li>
+            <li><Link to="/narwhal">Narwhal</Link></li>
+            <li><Link to="/whale">Whale</Link></li>
+            <li><Link to="/whale/beluga">Beluga Whale</Link></li>
+            <li><Link to="/whale/blue">Blue Whale</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/manatee">
             <Manatee />
           </Route>
           <Route path="/narwhal">
             <Narwhal />
           </Route>
+          <Route exact path="/whale">
+            <Whale />
+          </Route>
           <Route path="/whale">
             <Whale />
           </Route>
+        </Switch>
       </BrowserRouter>
-
     </div>
   );
 }
